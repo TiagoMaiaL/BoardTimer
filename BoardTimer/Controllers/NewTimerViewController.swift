33d1ab00679f0382c101972a5e06db18d29bfc79
Tiago Maia <tiago.maia.lopes@gmail.com>
2017-12-15 18:56:28 -0200

    Finishing carthage setup.

--- BoardTimer/Controllers/NewTimerViewController.swift
+++ BoardTimer/Controllers/NewTimerViewController.swift
@@ -7,14 +7,11 @@
 //
 
 import UIKit
+import Eureka
 
-class NewTimerViewController: UIViewController {
+class NewTimerViewController: FormViewController {
 
   // MARK: Properties
-  
-  @IBOutlet weak var minutesTextField: UITextField!
-  @IBOutlet weak var increaseTextField: UITextField!
-  @IBOutlet weak var modesSegmentedControl: UISegmentedControl!
   
   let timerStorage = TimerConfigurationStorage()
   
@@ -23,26 +20,41 @@ class NewTimerViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.largeTitleDisplayMode = .never
+    
+    form +++ Section("Section1")
+      <<< TextRow(){ row in
+        row.title = "Text Row"
+        row.placeholder = "Enter text here"
+      }
+      <<< PhoneRow(){
+        $0.title = "Phone Row"
+        $0.placeholder = "And numbers here"
+      }
+      +++ Section("Section2")
+      <<< DateRow(){
+        $0.title = "Date Row"
+        $0.value = Date(timeIntervalSinceReferenceDate: 0)
+    }
   }
 
   // MARK: Actions
   
-  @IBAction func doneTapped(_ sender: UIButton) {
-    guard let timeAmount = TimeInterval(minutesTextField.text ?? "") else { return }
-    guard let delay = TimeInterval(increaseTextField.text ?? "") else { return }
-    guard let mode = TimerMode(rawValue: modesSegmentedControl.selectedSegmentIndex) else { return }
-
-    let config = TimerConfiguration(time: timeAmount,
-                                    delay: delay,
-                                    mode: mode,
-                                    name: "")
-    timerStorage.store(config)
-
-    dismiss(animated: true) {
-      NotificationCenter.default.post(name: NotificationName.newTimer,
-                                      object: self,
-                                      userInfo: ["timer_config" : config])
-    }
-  }
+//  @IBAction func doneTapped(_ sender: UIButton) {
+//    guard let timeAmount = TimeInterval(minutesTextField.text ?? "") else { return }
+//    guard let delay = TimeInterval(increaseTextField.text ?? "") else { return }
+//    guard let mode = TimerMode(rawValue: modesSegmentedControl.selectedSegmentIndex) else { return }
+//
+//    let config = TimerConfiguration(time: timeAmount,
+//                                    delay: delay,
+//                                    mode: mode,
+//                                    name: "")
+//    timerStorage.store(config)
+//
+//    dismiss(animated: true) {
+//      NotificationCenter.default.post(name: NotificationName.newTimer,
+//                                      object: self,
+//                                      userInfo: ["timer_config" : config])
+//    }
+//  }
   
 }
